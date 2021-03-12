2412f5da5df35f9ee6601ce4d2b2faeebb4fa93a
Sandra <somoruyi@icloud.com>
2021-03-12 17:48:13 -0600

    adding maven project

---
+++ NoFlings/src/main/java/com/demo/noflings/NoFlingsApplication.java
@@ -0,0 +1,13 @@
+package com.demo.noflings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoFlingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoFlingsApplication.class, args);
+	}
+
+}
---
+++ NoFlings/src/test/java/com/demo/noflings/NoFlingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.noflings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoFlingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
